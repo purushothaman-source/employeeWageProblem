da8c78a7aca4f6fa7966ba142d8341405d124696
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-14 21:17:45 +0530

    ADDED :welcome message

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,9 @@
+package com.employeewage.problems;
+
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("*******WELCOME TO EMPLOYEEWAGE COMPUTATION PROBLEM*******");
+	}
+
+}
