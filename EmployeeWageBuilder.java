fd250206f9535f7fc89ac16674beca96ddc321db
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-14 22:01:33 +0530

    ADDED :emphrs variable to calculate employeeWage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -8,13 +8,20 @@ public class EmployeeWageBuilder {
 		System.out.println("*******WELCOME TO EMPLOYEEWAGE COMPUTATION PROBLEM*******");
 		//constants
 		final int ISPRESENT=1;
+		final int EmpRatePerHour=20;
+		int empHrs;
 		//random class
 		Random random=new Random();
 		//generating Random variable
 		int empCheck=random.nextInt(2);
-		if(empCheck==ISPRESENT)
+		if(empCheck==ISPRESENT) {
 			System.out.println("EMPLOYEE IS PRESENT");
-		else 
+			empHrs=8;
+		}
+		else {
 			System.out.println("EMPLOYEE IS ABSENT");
- }
+            empHrs=0;
+		}
+		System.out.println("Employeewage = "+empHrs*EmpRatePerHour);
+	}
 }
