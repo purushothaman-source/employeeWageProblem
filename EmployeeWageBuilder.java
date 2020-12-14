4a0a940960aaad05ff856508676392790872c91b
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-14 21:36:14 +0530

    ADDED:checking EmployeeAttendance

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,9 +1,20 @@
 package com.employeewage.problems;
 
+import java.util.Random;
+
 public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		System.out.println("*******WELCOME TO EMPLOYEEWAGE COMPUTATION PROBLEM*******");
-	}
-
+		//constants
+		final int ISPRESENT=1;
+		//random class
+		Random random=new Random();
+		//generating Random variable
+		int empCheck=random.nextInt(2);
+		if(empCheck==ISPRESENT)
+			System.out.println("EMPLOYEE IS PRESENT");
+		else 
+			System.out.println("EMPLOYEE IS ABSENT");
+ }
 }
