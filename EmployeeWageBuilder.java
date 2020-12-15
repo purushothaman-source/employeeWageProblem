e588e526c9d9e2e3c12283cf1c5d988206d68235
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-15 07:51:34 +0530

    ADDED:parttime

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -8,15 +8,20 @@ public class EmployeeWageBuilder {
 		System.out.println("*******WELCOME TO EMPLOYEEWAGE COMPUTATION PROBLEM*******");
 		//constants
 		final int ISPRESENT=1;
+		final int ISPARTTIME=2;
 		final int EmpRatePerHour=20;
 		int empHrs;
 		//random class
 		Random random=new Random();
 		//generating Random variable
-		int empCheck=random.nextInt(2);
+		int empCheck=random.nextInt(3);
 		if(empCheck==ISPRESENT) {
-			System.out.println("EMPLOYEE IS PRESENT");
+			System.out.println("EMPLOYEE IS PRESENT FULLTIME");
 			empHrs=8;
+		}
+		else if (empCheck==ISPARTTIME) {
+			System.out.println("EMPLOYEE IS PART TIME");
+			empHrs=4;
 		}
 		else {
 			System.out.println("EMPLOYEE IS ABSENT");
