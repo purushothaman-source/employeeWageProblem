32d58dbf1a47ba9dee200f3c67332d39fc5d1d7d
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-15 08:33:05 +0530

    REFRACTORED:used to while loop to continue the program until condition satisfy

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,7 +2,7 @@ package com.employeewage.problems;
 
 import java.util.Random;
 
-public class EmployeeWageBuilder {
+public class EmployeeWageBuilder  {
 
 	public static void main(String[] args) {
 		System.out.println("*******WELCOME TO EMPLOYEEWAGE COMPUTATION PROBLEM*******");
@@ -11,24 +11,36 @@ public class EmployeeWageBuilder {
 		final int ISPARTTIME=2;
 		final int EmpRatePerHour=20;
 		int empHrs;
+		final int TOTAL_EMP_HRS=100;
+		final int TOTAL_WORKING_DAYS=20;
+		int workingDays=0;
+		int totalworkinghours=0;
+		
+		
 		//random class
 		Random random=new Random();
+		while(totalworkinghours<=TOTAL_EMP_HRS && workingDays<TOTAL_WORKING_DAYS) {
+			
 		//generating Random variable
 		int empCheck=random.nextInt(3);
 		//case statement
 		switch(empCheck) {
 		case ISPRESENT: 
-			System.out.println("EMPLOYEE IS FULLTIME");
 			empHrs=8;
 	        break;	
 		case ISPARTTIME: 
-			System.out.println("EMPLOYEE IS PART TIME");
 			empHrs=4;
 		    break;
 		default:
-			System.out.println("EMPLOYEE IS ABSENT");
             empHrs=0;
 		}
-		System.out.println("Employeewage = "+empHrs*EmpRatePerHour);
+		workingDays++;
+		totalworkinghours+=empHrs;
+		
+		
+	}
+	   System.out.println("TOTALWORKINGHOURS : "+totalworkinghours);	
+	   System.out.println("workingDays : "+workingDays);
+		System.out.println("Employeewage = "+totalworkinghours*EmpRatePerHour);
 	}
 }
